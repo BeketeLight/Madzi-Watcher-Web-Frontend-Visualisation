3b43158fcd8c10e07f665277bdb13d85981a5481
BeketeLight <beketelight15@gmail.com>
2026-03-10 01:20:22 +0200

    feat: initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import AppRoutes from "./routes";
 function App() {
   return (
     <>
+    <div className="h-screen flex items-center justify-center bg-gray-100">
       <AppRoutes />
+    </div>
     </>
   );
 }
