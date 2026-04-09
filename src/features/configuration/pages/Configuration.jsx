e8b6964944bed52c951e09b1f993e05c6d0b42dc
BeketeLight <beketelight15@gmail.com>
2026-04-09 20:44:08 +0200

    configuration feature has been added

---
+++ src/features/configuration/pages/Configuration.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Configuration() {
+  return (
+    <div>
+      <h1>Configuration</h1>
+    </div>
+  );
+}   
---
+++ src/features/dashboard/pages/Configuration.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Configuration from "@/features/configuration/pages/Configuration";
+
+export default function DeviceConfiguration() {
+  return (
+    <Configuration/>
+  );
+}
