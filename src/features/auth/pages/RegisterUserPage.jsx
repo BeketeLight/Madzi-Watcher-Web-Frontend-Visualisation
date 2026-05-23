cbf5011d8b7198df4dd808726c96af567bd5b2c2
TerrenceKabango <bsc-42-19@unima.ac.mw>
2026-05-23 18:34:28 +0200

    create register water monitor component

--- src/features/auth/pages/RegisterUserPage.jsx
+++ src/features/auth/pages/RegisterUserPage.jsx
@@ -1,10 +1,7 @@
-import { RegisterUser } from "../components/RegisterUser";  
-
-export default function RegisterUserPage() {
+export default function RegisterUser() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      { <RegisterUser onSubmit={(data) => console.log('Register data:', data)} /> }
-      <h1>Register User</h1>
+    <div>
+      <h2>Register Water Monitor</h2>
     </div>
-  )
-}       
+  );
+}
