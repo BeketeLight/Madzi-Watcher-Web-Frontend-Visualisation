f1e604ec2f9ea793f815c559dcaa28eab13d9147
TerrenceKabango <bsc-42-19@unima.ac.mw>
2026-05-28 11:47:00 +0200

    fixed the delete option to be working

--- src/features/watermonitor/components/Watermonitor.jsx
+++ src/features/watermonitor/components/Watermonitor.jsx
@@ -99,7 +99,8 @@ export default function WaterMonitorsPage() {
             </div>
 
             <button
-              onClick={() => handleDelete(m.id)}
+              key={m._id}
+              onClick={() => handleDelete(m._id)}
               className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg"
             >
               Delete
